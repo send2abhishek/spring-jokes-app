238f4a22e6720542e599071b65d4cab2f880fe2e
Abhishek Kumar <Abhishek.kumar4@walmart.com>
2022-11-04 16:08:38 +0530

    feat: create joke controller

---
+++ src/main/java/com/app/jokes/jokesapp/controlers/JokeController.java
@@ -0,0 +1,24 @@
+package com.app.jokes.jokesapp.controlers;
+
+
+import com.app.jokes.jokesapp.services.JokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokesService service;
+
+    public JokeController(JokesService service) {
+        this.service = service;
+    }
+
+    @GetMapping({"/",""})
+    public String getJoke(Model model) {
+
+        model.addAttribute("joke", service.getJoke());
+        return "index";
+    }
+}
